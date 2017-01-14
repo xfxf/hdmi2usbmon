bbe5ee3c992ec6a1a32f7ec04e59dbc4614142a3
Ryan Verner <ryan.verner@gmail.com>
2017-01-14 19:13:33 +1100

    Refactor to use file-like handler instead

--- logger_basic.py
+++ logger_basic.py
@@ -4,23 +4,32 @@ Requires 'nextgen' hdmi2usb firmware post 15 Jan 2016.
 
 Super early hacky version.  Currently does not handle socket disconnects.
 """
-
+import sys
 import logging
 from logging.handlers import TimedRotatingFileHandler
 from hdmi2usbmon.device import Hdmi2UsbDevice
 
 
 def log_setup(log_filename):
-    log_handler = TimedRotatingFileHandler(log_filename,
-                                           when="h",
-                                           interval=1)
-    formatter = logging.Formatter(
+    """
+    Sets up logging for hourly rotating files in specified location, and stdout
+    """
+    format = logging.Formatter(
         '%(asctime)s hdmi2usbd [%(process)d]: %(message)s',
         '%b %d %H:%M:%S')
-    log_handler.setFormatter(formatter)
+
     logger = logging.getLogger()
-    logger.addHandler(log_handler)
     logger.setLevel(logging.INFO)
+
+    logfile_handler = TimedRotatingFileHandler(log_filename,
+                                           when="h",
+                                           interval=1)
+    logfile_handler.setFormatter(format)
+    logger.addHandler(logfile_handler)
+
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(format)
+    logger.addHandler(stdout_handler)
 
 
 if __name__ == "__main__":
@@ -31,31 +40,26 @@ if __name__ == "__main__":
     h.connect()
     print("Connected.")
 
-    h.sock.send(b'debug input0 on\r\n')
-    h.sock.send(b'debug input1 on\r\n')
-    h.sock.send(b'status short on\r\n')
+    device = h.sock.makefile(mode='rwb')
 
-    while True:
+    device.write(b'debug input0 off\r\n')
+    device.write(b'debug input1 off\r\n')
+    device.write(b'status short off\r\n')
+    device.flush()
 
-        buffer = b''
-
-        # get data until no more available
+    while h.connected:
         try:
-            while True:
-                buffer += h.sock.recv(512)
-        except BlockingIOError:
+            line = device.readline()
+        except OSError:
+            line = None
+
+        if line:
+            line = line.strip()
+            try:
+                logging.info(line.decode('ascii'))
+            except UnicodeDecodeError:
+                # non-ascii byte encountered (i.e. a ^C)
+                logging.info(line)
+        else:
             pass
-
-        # log each line into a file
-        if buffer:
-            lines = buffer.split(b'\r\n')
-            for line in lines:
-                if line:
-                    print(line)
-                    try:
-                        logging.info(line.decode('ascii'))
-                    except UnicodeDecodeError:
-                        # non-ascii byte encountered (i.e. a ^C)
-                        logging.info(line)
-
 
